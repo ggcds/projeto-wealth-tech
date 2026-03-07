f309fae915405df8faf883fda2c28ba9713ab504
Guilherme Caldas <guigcds@icloud.com>
2026-03-07 00:13:30 -0300

    docs: simplifica comentarios e documenta logica de negocio

--- invest_analytics/macros/generate_schema_name.sql
+++ invest_analytics/macros/generate_schema_name.sql
@@ -1,8 +1,12 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
+    {# Define o schema padrão configurado no seu profile (ex: 'silver') #}
     {%- set default_schema = target.schema -%}
+
+    {# Se não houver um schema customizado no dbt_project, usa o padrão #}
     {%- if custom_schema_name is none -%}
         {{ default_schema }}
     {%- else -%}
+        {# Se houver (ex: 'gold'), usa apenas o nome customizado, sem prefixos #}
         {{ custom_schema_name | trim }}
     {%- endif -%}
 {%- endmacro %}
